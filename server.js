633235858e158075ff46d6639956462778bc59a9
Blessing Orazulume <blessing.orazulume@andela.com>
2018-04-27 19:41:43 +0100

    fix cors error

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ dbConn.once('open', () => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 app.get('/', (req, res) => {
     res.json({ messge: 'Welcome to Note API'})
 });
