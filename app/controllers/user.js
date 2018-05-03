7165775c81b634cbe3ea6bf50a9950eec57a3e53
Blessing Orazulume <blessing.orazulume@andela.com>
2018-05-03 11:50:37 +0100

    fixed crash when user types in incorrect password

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -23,9 +23,9 @@ exports.create = (req, res) => {
 exports.login = (req, res) => {
     User.findOne({ email: req.body.email }, (err, user) => {
         if (err) res.status(500).send({ message: `Cannot find user with email: ${req.body.email}` });
-        if (!user) res.status(404).send('No user found.');
+        if (!user) return res.status(404).send('No user found.');
         user.comparePassword(req.body.password, (err, isMatch) => {
-            if (err || isMatch === false) res.status(500).send({ message: `You cannot login` });
+            if (err || isMatch === false) return res.status(500).send({ message: `You cannot login` });
             const token = jwt.sign({ id: user._id }, process.env.SECRET, {
                 expiresIn: 86400 // expires in 24 hours
             });
